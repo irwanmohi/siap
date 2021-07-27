2e152b4f3de72a111a715ef2404fe94b7919e122
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-07-27 16:39:28 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /usr/bin
+wget -q bannerku "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/bannerku"
+wget -q -O menu "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/menu.sh"
+wget -q -O bbr "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/bbr.sh"
+wget -q -O ssh "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/ssh.sh"
+wget -q -O ssssr "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/ssssr.sh"
+wget -q -O trojaan "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/trojaan.sh"
+wget -q -O v2raay "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/v2raay.sh"
+wget -q -O vleess "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/vleess.sh"
+wget -q -O wgr "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/wgr.sh"
+chmod +x bannerku && sed -i -e 's/\r$//' bannerku
+chmod +x menu && sed -i -e 's/\r$//' menu
+chmod +x bbr && sed -i -e 's/\r$//' bbr
+chmod +x ssh  && sed -i -e 's/\r$//' ssh
+chmod +x ssssr && sed -i -e 's/\r$//' ssssr
+chmod +x trojaan && sed -i -e 's/\r$//' trojaan
+chmod +x v2raay && sed -i -e 's/\r$//' v2raay
+chmod +x vleess && sed -i -e 's/\r$//' vleess
+chmod +x wgr && sed -i -e 's/\r$//' wgr
