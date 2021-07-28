e5fb6fb45c82d94636148b2d94cc275de0b0bbda
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-07-28 14:13:00 +0800

    Update trojaan.sh

--- trojaan.sh
+++ trojaan.sh
@@ -2,17 +2,9 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/siap/beta/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
+
 clear
+cat /usr/bin/bannerku | lolcat
 echo -e "\e[32m════════════════════════════════════════\e[m" | lolcat
 echo -e "             ═══[Trojan]═══"
 echo -e "\e[32m════════════════════════════════════════\e[m" | lolcat
